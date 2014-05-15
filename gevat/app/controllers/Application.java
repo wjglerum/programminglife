2661ec6046d0aa388220d3f8f286f9a8eabdcab2
Jasper Denkers <git@jasperdenkers.com>
2014-05-15 13:24:26 +0200

    Fixed test

--- gevat/app/controllers/Application.java
+++ gevat/app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok("hello special world");
+        return ok(index.render("Your new application is ready."));
     }
 
 }
