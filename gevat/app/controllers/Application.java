99a53098e4575adfddb1519fabb389cf8a96cb24
Jasper Denkers <git@jasperdenkers.com>
2014-05-15 15:24:28 +0200

    Rerouted index to dashboard and added about and help pages

--- gevat/app/controllers/Application.java
+++ gevat/app/controllers/Application.java
@@ -7,8 +7,20 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(dashboard.render("Welcome to GEVATT!"));
-    }
+  public static Result index() {
+    return redirect("/dashboard");
+  }
+
+  public static Result dashboard() {
+    return ok(dashboard.render("Welcome to GEVATT!"));
+  }
+
+  public static Result help() {
+    return ok(help.render());
+  }
+
+  public static Result about() {
+    return ok(about.render());
+  }
 
 }
