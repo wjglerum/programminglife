f398d131defa7d59c06589f883befa285d761436
Jasper Denkers <git@jasperdenkers.com>
2014-05-20 11:47:55 +0200

    Clickable rows only on body rows (not header)

--- gevat/public/javascripts/main.js
+++ gevat/public/javascripts/main.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
 	// Make table rows clickable as links by using the href attribute of <tr> element
-	$(".clickable-rows tr").click(function() {
+	$(".clickable-rows tbody tr").click(function() {
 		window.document.location = $(this).attr("href");
 	});
 });
