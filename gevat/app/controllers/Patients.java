dc7835b6e7b2b76ccff6a4a0da06a6c5e8ac8b18
Jasper Denkers <git@jasperdenkers.com>
2014-05-20 12:07:39 +0200

    While requesting a patient check if it's found and display an error and the patient overview if it's not

--- gevat/app/controllers/Patients.java
+++ gevat/app/controllers/Patients.java
@@ -15,6 +15,14 @@ public class Patients extends Controller {
   public static Result show(int id) {
     Patient p = Patient.get(id);
     
+    // Return to the patients overview and display a message the requested patient isn't found
+    if (p == null) {
+      flash("patient-not-found", "The requested patient could not be found. Please select another one below.");
+      
+      return notFound(patients.render(Patient.getAll(), Application.getUser()));
+    }
+    
+    // Render the patient otherwise
     return ok(patient.render(p, Application.getUser()));
   }
 
