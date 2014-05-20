0b2700764d29940ef37945ca4715da25414c30c0
Jasper Denkers <git@jasperdenkers.com>
2014-05-20 11:45:16 +0200

    Added table view in patients overview. Added javascript so table rows are clickable and link to individual patient pages. Changed Patient attributes to public and removed bsn and added id

--- gevat/app/models/Patient.java
+++ gevat/app/models/Patient.java
@@ -2,37 +2,14 @@ package models;
 
 public class Patient {
 
-	private String name;
-	private String surname;
-	private int bsn;
+  public int id;
+	public String name;
+	public String surname;
 
-	public Patient(String name, String surname, int bsn) {
+	public Patient(int id, String name, String surname) {
+    this.id = id;
 		this.name = name;
 		this.surname = surname;
-		this.bsn = bsn;
 	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getSurname() {
-		return surname;
-	}
-
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-
-	public int getBsn() {
-		return bsn;
-	}
-
-	public void setBsn(int bsn) {
-		this.bsn = bsn;
-	}
+	
 }
