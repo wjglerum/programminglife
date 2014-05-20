b7a43a3efe51e4c6366b08611cced8fbd77306ca
Jasper Denkers <git@jasperdenkers.com>
2014-05-20 11:20:38 +0200

    Added pages to view one or all patients. Also the application sidebar menu now highlights the selected nav item. Patients controller added to query patients (for now just dummy patients)

---
+++ gevat/app/models/Patient.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Patient {
+
+  public String name;
+  public int id;
+  
+  public Patient(String name, int id) {
+    this.name = name;
+    this.id = id;
+  }
+  
+}
