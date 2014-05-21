a440de15c59f962927cf5817a69a5af76124f717
Jasper Denkers <git@jasperdenkers.com>
2014-05-21 11:02:56 +0200

    Make clickable table rows unclickable if they show 'not found' messages

--- gevat/public/javascripts/main.js
+++ gevat/public/javascripts/main.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
 	// Make table rows clickable as links by using the href attribute of <tr> element
-	$(".clickable-rows tbody tr").click(function() {
+	$(".clickable-rows tbody tr").not(".not-clickable").click(function() {
 		window.document.location = $(this).attr("href");
 	});
 });
