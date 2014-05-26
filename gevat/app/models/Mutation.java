7a317509623e273c3afe39d80699237aa86426c1
robbertvs <robbertvs@gmail.com>
2014-05-26 10:51:15 +0200

    Added javadoc to Mutation and VCFReader

--- gevat/app/models/Mutation.java
+++ gevat/app/models/Mutation.java
@@ -12,13 +12,7 @@ import org.broadinstitute.variant.variantcontext.VariantContext;
 
 public class Mutation extends VariantContext {
 
-    public String mutationType;
-/*
-    protected Mutation(String source, String ID, String contig, long start, long stop, Collection<Allele> alleles, GenotypesContext genotypes,
-                         double log10PError, Set<String> filters, Map<String,Object> attributes,
-                         boolean fullyDecoded, EnumSet<VariantContext.Validation> validationToPerform) {
-        super(source, ID, contig, start, stop, alleles, genotypes, log10PError, filters, attributes, fullyDecoded, validationToPerform);
-    }*/
+	public String mutationType;
 
 	/**
 	 * Store information about a mutation
@@ -31,41 +25,41 @@ public class Mutation extends VariantContext {
 	 */
 	public Mutation(int id, String mutationType, String rsID, int chromosome,
 			Collection<Allele> alleles, GenotypesContext genotypes) {
-        super(null, rsID, Integer.toString(chromosome), id, id, alleles, genotypes, 0, null, null, false, EnumSet.noneOf(Validation.class));
-        this.mutationType=mutationType;
+		super(null, rsID, Integer.toString(chromosome), id, id, alleles,
+				genotypes, 0, null, null, false, EnumSet
+						.noneOf(Validation.class));
+		this.mutationType = mutationType;
 	}
 
-   public Mutation(int id, String mutationType, String rsID, int chromosome,
-           char[] alleles) {
-       super(null, rsID, Integer.toString(chromosome), id, id, toAlleleCollection(new String(alleles)), toGenotypesContext(new String(alleles)), 0, null, null, false, EnumSet.noneOf(Validation.class));
-       this.mutationType=mutationType;
-   }
+	public Mutation(int id, String mutationType, String rsID, int chromosome,
+			char[] alleles) {
+		super(null, rsID, Integer.toString(chromosome), id, id,
+				toAlleleCollection(new String(alleles)),
+				toGenotypesContext(new String(alleles)), 0, null, null, false,
+				EnumSet.noneOf(Validation.class));
+		this.mutationType = mutationType;
+	}
 
-    public Mutation(VariantContext vc) {
-        super(vc);
-        
-        this.mutationType = "SNP";
-    }
-    
-    public String getMutationType()
-    {
-        return mutationType;
-    }
-    
-    public String getRsID()
-    {
-        return getID();
-    }
-    
-    public long getId()
-    {
-        return start;
-    }
-    
-    public int getChromosome()
-    {
-        return Integer.parseInt(contig);
-    }
+	public Mutation(VariantContext vc, String mutationType) {
+		super(vc);
+		this.mutationType = mutationType;
+	}
+
+	public String getMutationType() {
+		return mutationType;
+	}
+
+	public String getRsID() {
+		return getID();
+	}
+
+	public long getId() {
+		return start;
+	}
+
+	public int getChromosome() {
+		return Integer.parseInt(contig);
+	}
 
 	/**
 	 * Return the alleles of the child
@@ -73,7 +67,7 @@ public class Mutation extends VariantContext {
 	 * @return String
 	 */
 	public String child() {
-        return toBaseString(getAlleles());
+		return toBaseString(getAlleles());
 	}
 
 	/**
@@ -82,7 +76,7 @@ public class Mutation extends VariantContext {
 	 * @return String
 	 */
 	public String father() {
-        return toBaseString(getGenotype("FATHER").getAlleles());
+		return toBaseString(getGenotype("FATHER").getAlleles());
 	}
 
 	/**
@@ -91,19 +85,25 @@ public class Mutation extends VariantContext {
 	 * @return String
 	 */
 	public String mother() {
-        return toBaseString(getGenotype("MOTHER").getAlleles());
+		return toBaseString(getGenotype("MOTHER").getAlleles());
 	}
-	
-	public String toBaseString(List<Allele> alleles) {
-	    return "[" + alleles.get(0).getBaseString() + ", " + alleles.get(1).getBaseString() + "]";
+
+	protected String toBaseString(List<Allele> alleles) {
+		return "[" + alleles.get(0).getBaseString() + ", "
+				+ alleles.get(1).getBaseString() + "]";
 	}
-	
-	public String toAllelesString() {
-	  List<Allele> childAlleles = getAlleles();
-	  List<Allele> fatherAlleles = getGenotype("FATHER").getAlleles();
-	  List<Allele> motherAlleles = getGenotype("MOTHER").getAlleles();
-	  
-	  return childAlleles.get(0).getBaseString() + childAlleles.get(1).getBaseString() + fatherAlleles.get(0).getBaseString() + fatherAlleles.get(1).getBaseString() + motherAlleles.get(0).getBaseString() + motherAlleles.get(1).getBaseString();
+
+	protected String toAllelesString() {
+		List<Allele> childAlleles = getAlleles();
+		List<Allele> fatherAlleles = getGenotype("FATHER").getAlleles();
+		List<Allele> motherAlleles = getGenotype("MOTHER").getAlleles();
+
+		return childAlleles.get(0).getBaseString()
+				+ childAlleles.get(1).getBaseString()
+				+ fatherAlleles.get(0).getBaseString()
+				+ fatherAlleles.get(1).getBaseString()
+				+ motherAlleles.get(0).getBaseString()
+				+ motherAlleles.get(1).getBaseString();
 	}
 
 	/**
@@ -123,39 +123,52 @@ public class Mutation extends VariantContext {
 			String sort = rs.getString("sort");
 			String rsID = rs.getString("rsID");
 			int chromosome = rs.getInt("chromosome");
-			Collection<Allele> alleles = toAlleleCollection(rs.getString("alleles"));
-			GenotypesContext genotypescontext = toGenotypesContext(rs.getString("alleles"));
-			mutations.add(new Mutation(id, sort, rsID, chromosome, alleles, genotypescontext));
+			Collection<Allele> alleles = toAlleleCollection(rs
+					.getString("alleles"));
+			GenotypesContext genotypescontext = toGenotypesContext(rs
+					.getString("alleles"));
+			mutations.add(new Mutation(id, sort, rsID, chromosome, alleles,
+					genotypescontext));
 		}
 		return mutations;
 	}
-	
-	public static Collection<Allele> toAlleleCollection(String allelesString)
-	{
-	    Collection<Allele> alleles = new ArrayList<Allele>();
-        alleles.add(toAllele(allelesString.substring(0,1), true));
-        alleles.add(toAllele(allelesString.substring(1,2), false));
-	    return alleles;
+
+	/**
+	 * Makes an allele collection of the alleles of the child (the first two
+	 * alleles in the string)
+	 */
+	protected static Collection<Allele> toAlleleCollection(String allelesString) {
+		Collection<Allele> alleles = new ArrayList<Allele>();
+		alleles.add(toAllele(allelesString.substring(0, 1), true));
+		alleles.add(toAllele(allelesString.substring(1, 2), false));
+		return alleles;
 	}
-	
-	public static GenotypesContext toGenotypesContext(String s)
-	{
-	    GenotypesContext gc = GenotypesContext.create();
-        gc.add(toGenotype(s.substring(2,4), "FATHER"));
-        gc.add(toGenotype(s.substring(4,6), "MOTHER"));
-        return gc;
+
+	/**
+	 * Makes a GenotypesContext of the alleles of the parents using the 3rd to
+	 * 6th alleles in the string
+	 */
+	protected static GenotypesContext toGenotypesContext(String s) {
+		GenotypesContext gc = GenotypesContext.create();
+		gc.add(toGenotype(s.substring(2, 4), "FATHER"));
+		gc.add(toGenotype(s.substring(4, 6), "MOTHER"));
+		return gc;
 	}
-	
-	public static Genotype toGenotype(String allelesString, String name)
-	{
-	    List<Allele> alleles = new ArrayList<Allele>();
-        alleles.add(toAllele(allelesString.substring(0,1), false));
-        alleles.add(toAllele(allelesString.substring(1,2), false));
-        return GenotypeBuilder.create(name, alleles);
+
+	/**
+	 * Makes a Genotype of a 2-letter string representing two alleles
+	 */
+	protected static Genotype toGenotype(String allelesString, String name) {
+		List<Allele> alleles = new ArrayList<Allele>();
+		alleles.add(toAllele(allelesString.substring(0, 1), false));
+		alleles.add(toAllele(allelesString.substring(1, 2), false));
+		return GenotypeBuilder.create(name, alleles);
 	}
-	
-	public static Allele toAllele(String s, boolean isref)
-	{
-	    return Allele.create(s, isref);
+
+	/**
+	 * Creates an allele from the given string
+	 */
+	protected static Allele toAllele(String s, boolean isref) {
+		return Allele.create(s, isref);
 	}
 }
--- gevat/app/models/VCFReader.java
+++ gevat/app/models/VCFReader.java
@@ -12,60 +12,66 @@ import org.broadinstitute.variant.variantcontext.VariantContext;
 import org.broadinstitute.variant.vcf.VCFCodec;
 
 public class VCFReader {
-    public static void main(String args[])
-    {
-        getMutations("H:/vakken/anonymous_trio1.vcf");
-    }
-    
-    public static List<Mutation> getMutations(String fileName)
-    {
-        FeatureReader<VariantContext> fr = AbstractFeatureReader.getFeatureReader(fileName, new VCFCodec(), false);
-        fr.getHeader();
-        List<Mutation> SNPList = getMutations(fr);
-        try {
-            fr.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return SNPList;
-    }
-    
-    public static List<Mutation> getMutations(FeatureReader<VariantContext> fr)
-    {
-        List<Mutation> SNPList = new ArrayList<Mutation>();
-        try {
-            Iterator<VariantContext> it;
-            it = fr.iterator();
-            while( it.hasNext() )
-            {
-                VariantContext vc = it.next();
-                if(hasMutation(vc))
-                {
-                    SNPList.add(toMutation(vc));
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return SNPList;
-    }
-    
-    public static boolean hasMutation(VariantContext vc)
-    {
-//      TODO: take names like "DAUGHTER" from file 
-        List<Allele> D = vc.getGenotype("DAUGHTER").getAlleles();
-        List<Allele> F = vc.getGenotype("FATHER").getAlleles();
-        List<Allele> M = vc.getGenotype("MOTHER").getAlleles();
-        return !possibleAlleleSet(D, F, M);
-    }
-    
-    public static Mutation toMutation(VariantContext vc)
-    {
-        return new Mutation(vc);
-    }
-    
-    public static boolean possibleAlleleSet(List<Allele> D, List<Allele> F, List<Allele> M)
-    {
-        return ((F.contains(D.get(0)) && M.contains(D.get(1))) || (F.contains(D.get(1)) && M.contains(D.get(0))));
-    }
+	/**
+	 * Return a list of mutations found in the file.
+	 */
+	public static List<Mutation> getMutations(String fileName) {
+		FeatureReader<VariantContext> fr = AbstractFeatureReader
+				.getFeatureReader(fileName, new VCFCodec(), false);
+		fr.getHeader();
+		List<Mutation> SNPList = getMutations(fr);
+		try {
+			fr.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return SNPList;
+	}
+
+	protected static List<Mutation> getMutations(
+			FeatureReader<VariantContext> fr) {
+		List<Mutation> SNPList = new ArrayList<Mutation>();
+		try {
+			Iterator<VariantContext> it;
+			it = fr.iterator();
+			while (it.hasNext()) {
+				VariantContext vc = it.next();
+				if (hasMutation(vc)) {
+					SNPList.add(toMutation(vc, "SNP"));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return SNPList;
+	}
+
+	/**
+	 * Takes alleles from the child and parents and checks whether a mutation
+	 * must have taken place in order to get this allele combination
+	 */
+	public static boolean hasMutation(VariantContext vc) {
+		// TODO: take names like "DAUGHTER" from file
+		List<Allele> D = vc.getGenotype("DAUGHTER").getAlleles();
+		List<Allele> F = vc.getGenotype("FATHER").getAlleles();
+		List<Allele> M = vc.getGenotype("MOTHER").getAlleles();
+		return !possibleAlleleSet(D, F, M);
+	}
+
+	/**
+	 * Store variantcontext with a mutation as a mutation.
+	 */
+	public static Mutation toMutation(VariantContext vc, String mutationType) {
+		return new Mutation(vc, mutationType);
+	}
+
+	/**
+	 * Checks whether this is a possible allele set: one of the childs alleles
+	 * is from the father and one is from the mother.
+	 */
+	public static boolean possibleAlleleSet(List<Allele> D, List<Allele> F,
+			List<Allele> M) {
+		return ((F.contains(D.get(0)) && M.contains(D.get(1))) || (F.contains(D
+				.get(1)) && M.contains(D.get(0))));
+	}
 }
