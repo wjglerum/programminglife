267b194a56909e4ca698c386195750719b5ef5b8
Willem Jan Glerum <wj.glerum@gmail.com>
2014-05-26 11:44:18 +0200

    Added AuthenticationTest and UserTest and changed pom.xml

---
+++ gevat/test/test/controllers/AuthenticationTest.java
@@ -0,0 +1,27 @@
+package test.controllers;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.callAction;
+import static play.test.Helpers.charset;
+import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.contentType;
+import static play.test.Helpers.status;
+
+import org.junit.Test;
+
+import play.mvc.Result;
+import play.*;
+
+public class AuthenticationTest {
+
+	@Test
+	public void testLogin() {
+		Result result = callAction(controllers.routes.ref.Authentication.login());
+		assertThat(status(result)).isEqualTo(OK);
+		assertThat(contentType(result)).isEqualTo("text/html");
+		assertThat(charset(result)).isEqualTo("utf-8");
+		assertThat(contentAsString(result)).contains("Username");
+	}
+}
+
