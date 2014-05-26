ffd280c68fd831ef87566c1396d308bb279b40dc
Jasper Denkers <git@jasperdenkers.com>
2014-05-26 11:16:35 +0200

    Added session based view tests

---
+++ gevat/test/test/views/SessionTest.java
@@ -0,0 +1,43 @@
+package test.views;
+
+import models.User;
+
+import org.junit.*;
+
+import controllers.Authentication;
+import controllers.Authentication.Login;
+import play.mvc.*;
+import play.test.*;
+import play.api.templates.Html;
+import play.libs.F.*;
+import scala.collection.mutable.StringBuilder;
+import static play.data.Form.form;
+import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.*;
+
+public class SessionTest {
+  
+  private User user;
+  
+  @Before
+  public void fakeSession() {
+    user = new User(1, "Foo", "Bar", "foobar");
+  }
+
+  @Test
+  public void aboutTemplateLoggedIn() {
+    Content html = views.html.about.render(user);
+    
+    assertThat(contentType(html)).isEqualTo("text/html");
+    assertThat(contentAsString(html)).contains("Log out");
+  }
+
+  @Test
+  public void aboutTemplateLoggedOut() {
+    Content html = views.html.about.render(null);
+    
+    assertThat(contentType(html)).isEqualTo("text/html");
+    assertThat(contentAsString(html)).contains("Log in");
+  }
+
+}
