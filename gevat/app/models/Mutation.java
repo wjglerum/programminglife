256ad272f476be5333cda3c7f792f2f708fa21e1
Ruben <rubenbes94@gmail.com>
2014-05-26 12:11:57 +0200

    Checkstyle formatting Mutation.java

--- gevat/app/models/Mutation.java
+++ gevat/app/models/Mutation.java
@@ -2,7 +2,10 @@ package models;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.List;
 
 import org.broadinstitute.variant.variantcontext.Allele;
 import org.broadinstitute.variant.variantcontext.GenotypesContext;
@@ -10,93 +13,154 @@ import org.broadinstitute.variant.variantcontext.Genotype;
 import org.broadinstitute.variant.variantcontext.GenotypeBuilder;
 import org.broadinstitute.variant.variantcontext.VariantContext;
 
+/**
+ *
+ * @author rhvanstaveren
+ *
+ */
 public class Mutation extends VariantContext {
 
 	public String mutationType;
 
 	/**
-	 * Store information about a mutation
-	 * 
-	 * @param id
-	 * @param sort
-	 * @param rsID
-	 * @param chromosome
-	 * @param alleles
+	 * Stores information about a mutation.
+	 *
+	 * @param id The location of the allele
+	 * @param mutationType The type of mutation
+	 * @param rsID The id of the SNP
+	 * @param chromosome The chromosome the SNP is on
+	 * @param alleles The basepair of the child
+	 * @param genotypes The genotype of the parents
 	 */
-	public Mutation(int id, String mutationType, String rsID, int chromosome,
-			Collection<Allele> alleles, GenotypesContext genotypes) {
+	public Mutation(final int id, final String mutationType,
+			final String rsID, final int chromosome,
+			final Collection<Allele> alleles,
+			final GenotypesContext genotypes) {
 		super(null, rsID, Integer.toString(chromosome), id, id, alleles,
 				genotypes, 0, null, null, false, EnumSet
 						.noneOf(Validation.class));
 		this.mutationType = mutationType;
 	}
 
-	public Mutation(int id, String mutationType, String rsID, int chromosome,
-			char[] alleles) {
+	/**
+	 * Stores information about a mutation.
+	 *
+	 * @param id The location of the allele
+	 * @param mutationType The type of mutation
+	 * @param rsID The id of the SN
+	 * @param chromosome The chromosome the SNP is on
+	 * @param alleles The basepairs which need to be converted
+	 */
+	public Mutation(final int id, final String mutationType,
+			final String rsID, final int chromosome,
+			final char[] alleles) {
 		super(null, rsID, Integer.toString(chromosome), id, id,
 				toAlleleCollection(new String(alleles)),
-				toGenotypesContext(new String(alleles)), 0, null, null, false,
+				toGenotypesContext(new String(alleles)), 0,
+				null, null, false,
 				EnumSet.noneOf(Validation.class));
 		this.mutationType = mutationType;
 	}
 
-	public Mutation(VariantContext vc, String mutationType) {
+	/**
+	 * Stores information about a mutation.
+	 *
+	 * @param vc The VariantContext without a MutationType
+	 * @param mutationType The MutationType as String
+	 */
+	public Mutation(final VariantContext vc, final String mutationType) {
 		super(vc);
 		this.mutationType = mutationType;
 	}
 
-	public String getMutationType() {
-		return mutationType;
-	}
-
-	public String getRsID() {
-		return getID();
-	}
-
-	public long getId() {
-		return start;
-	}
-
-	public int getChromosome() {
-		return Integer.parseInt(contig);
+	/**
+	 * Gets the mutationType.
+	 *
+	 * @return Returns the String MutationType
+	 */
+	public final String getMutationType() {
+		return this.mutationType;
 	}
 
 	/**
-	 * Return the alleles of the child
-	 * 
-	 * @return String
+	 * Gets the rsID.
+	 *
+	 * @return String rsID
 	 */
-	public String child() {
-		return toBaseString(getAlleles());
+	public final String getRsID() {
+		return this.getID();
 	}
 
 	/**
-	 * Return the alleles of the father
-	 * 
-	 * @return String
+	 * Gets the id number as a long.
+	 *
+	 * @return The id
 	 */
-	public String father() {
-		return toBaseString(getGenotype("FATHER").getAlleles());
+	public final long getId() {
+		return this.start;
 	}
 
 	/**
-	 * Return the alleles of the mother
-	 * 
-	 * @return String
+	 * Gets the chromosome number.
+	 *
+	 * @return Returns the chromosome number.
 	 */
-	public String mother() {
-		return toBaseString(getGenotype("MOTHER").getAlleles());
+	public final int getChromosome() {
+		return Integer.parseInt(this.contig);
 	}
 
-	protected String toBaseString(List<Allele> alleles) {
+	/**
+	 * Return the alleles of the child.
+	 *
+	 * @return String with alleles of the child
+	 */
+	public final String child() {
+		return this.toBaseString(this.getAlleles());
+	}
+
+	/**
+	 * Return the alleles of the father.
+	 *
+	 * @return String
+	 */
+	public final String father() {
+		return this.toBaseString(
+				this.getGenotype("FATHER").getAlleles());
+	}
+
+	/**
+	 * Return the alleles of the mother.
+	 *
+	 * @return String
+	 */
+	public final String mother() {
+		return this.toBaseString(
+				this.getGenotype("MOTHER").getAlleles());
+	}
+
+	/**
+	 * Gives the basepair alleles as a String.
+	 *
+	 * @param alleles The list of alleles
+	 *
+	 * @return Returns the String representation of the basepair
+	 */
+	protected final String toBaseString(final List<Allele> alleles) {
 		return "[" + alleles.get(0).getBaseString() + ", "
 				+ alleles.get(1).getBaseString() + "]";
 	}
 
-	public String toAllelesString() {
-		List<Allele> childAlleles = getAlleles();
-		List<Allele> fatherAlleles = getGenotype("FATHER").getAlleles();
-		List<Allele> motherAlleles = getGenotype("MOTHER").getAlleles();
+	/**
+	 * Gives the alleles as a String.
+	 *
+	 * @return Returns the String representation of the basepair
+	 */
+	public final String toAllelesString() {
+		List<Allele> childAlleles = this.getAlleles();
+		List<Allele> fatherAlleles =
+				this.getGenotype("FATHER").getAlleles();
+		List<Allele> motherAlleles =
+				this.getGenotype("MOTHER").getAlleles();
 
 		return childAlleles.get(0).getBaseString()
 				+ childAlleles.get(1).getBaseString()
@@ -107,14 +171,18 @@ public class Mutation extends VariantContext {
 	}
 
 	/**
-	 * Retrive a list of all the mutations of a patient
-	 * 
-	 * @param p_id
-	 * @return List<Mutation>
-	 * @throws SQLException
+	 * Retrieve a list of all the mutations of a patient.
+	 *
+	 * @param pId The id
+	 *
+	 * @return Returns List<Mutation>, a list of all mutations
+	 *
+	 * @throws SQLException In case SQL goes wrong
 	 */
-	public static List<Mutation> getMutations(int p_id) throws SQLException {
-		String query = "SELECT * FROM mutations WHERE p_id = '" + p_id + "';";
+	public static List<Mutation> getMutations(final int pId)
+			throws SQLException {
+		String query = "SELECT * FROM mutations WHERE p_id = '"
+				+ pId + "';";
 		ResultSet rs = Database.select("data", query);
 		List<Mutation> mutations = new ArrayList<Mutation>();
 
@@ -125,9 +193,11 @@ public class Mutation extends VariantContext {
 			int chromosome = rs.getInt("chromosome");
 			Collection<Allele> alleles = toAlleleCollection(rs
 					.getString("alleles"));
-			GenotypesContext genotypescontext = toGenotypesContext(rs
+			GenotypesContext genotypescontext =
+					toGenotypesContext(rs
 					.getString("alleles"));
-			mutations.add(new Mutation(id, sort, rsID, chromosome, alleles,
+			mutations.add(new Mutation(id, sort,
+					rsID, chromosome, alleles,
 					genotypescontext));
 		}
 		return mutations;
@@ -135,9 +205,14 @@ public class Mutation extends VariantContext {
 
 	/**
 	 * Makes an allele collection of the alleles of the child (the first two
-	 * alleles in the string)
+	 * alleles in the string).
+	 *
+	 * @param allelesString The String of alleles
+	 *
+	 * @return Returns a collection of alleles
 	 */
-	protected static Collection<Allele> toAlleleCollection(String allelesString) {
+	protected static Collection<Allele> toAlleleCollection(
+			final String allelesString) {
 		Collection<Allele> alleles = new ArrayList<Allele>();
 		alleles.add(toAllele(allelesString.substring(0, 1), true));
 		alleles.add(toAllele(allelesString.substring(1, 2), false));
@@ -145,10 +220,14 @@ public class Mutation extends VariantContext {
 	}
 
 	/**
-	 * Makes a GenotypesContext of the alleles of the parents using the 3rd to
-	 * 6th alleles in the string
+	 * Makes a GenotypesContext of the alleles of the
+	 * parents using the 3rd to 6th alleles in the string.
+	 *
+	 * @param s The string used
+	 *
+	 * @return Returns as a GenotypesContext
 	 */
-	protected static GenotypesContext toGenotypesContext(String s) {
+	protected static GenotypesContext toGenotypesContext(final String s) {
 		GenotypesContext gc = GenotypesContext.create();
 		gc.add(toGenotype(s.substring(2, 4), "FATHER"));
 		gc.add(toGenotype(s.substring(4, 6), "MOTHER"));
@@ -156,9 +235,15 @@ public class Mutation extends VariantContext {
 	}
 
 	/**
-	 * Makes a Genotype of a 2-letter string representing two alleles
+	 * Makes a Genotype of a 2-letter string representing two alleles.
+	 *
+	 * @param allelesString The string of the alleles
+	 * @param name The name
+	 *
+	 * @return Returns a Genotype
 	 */
-	protected static Genotype toGenotype(String allelesString, String name) {
+	protected static Genotype toGenotype(final String allelesString,
+			final String name) {
 		List<Allele> alleles = new ArrayList<Allele>();
 		alleles.add(toAllele(allelesString.substring(0, 1), false));
 		alleles.add(toAllele(allelesString.substring(1, 2), false));
@@ -166,9 +251,14 @@ public class Mutation extends VariantContext {
 	}
 
 	/**
-	 * Creates an allele from the given string
+	 * Creates an allele from the given string.
+	 *
+	 * @param s The give string
+	 * @param isref If it is ref or not
+	 *
+	 * @return Returns an Allele
 	 */
-	protected static Allele toAllele(String s, boolean isref) {
+	protected static Allele toAllele(final String s, final boolean isref) {
 		return Allele.create(s, isref);
 	}
 }
