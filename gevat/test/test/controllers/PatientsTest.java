241fa3676815e3acfa8638eda917ee29479e3029
Mathijs Hoogland <mathijs@hoogland.it>
2014-05-27 12:23:28 +0200

    Finished patientstest, needs some refactoring

--- gevat/test/test/controllers/PatientsTest.java
+++ gevat/test/test/controllers/PatientsTest.java
@@ -1,6 +1,6 @@
 package test.controllers;
+
 import static org.fest.assertions.Assertions.assertThat;
-import static org.junit.Assert.assertNull;
 import static play.mvc.Http.Status.OK;
 import static play.test.Helpers.callAction;
 import static play.test.Helpers.contentAsString;
@@ -9,9 +9,13 @@ import static play.test.Helpers.fakeRequest;
 import static play.test.Helpers.running;
 import static play.test.Helpers.status;
 
+import static play.mvc.Http.Status.NOT_FOUND;
+import static play.mvc.Http.Status.BAD_REQUEST;
+
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import play.mvc.Http.Cookie;
@@ -19,6 +23,9 @@ import play.mvc.Result;
 import controllers.Patients.Add;
 
 public class PatientsTest {
+
+	private Result result;
+	private Cookie playSession;
 
 	/**
 	 * Check if we get an an error message if a name is not set
@@ -32,7 +39,7 @@ public class PatientsTest {
 	}
 
 	/**
-	 * Check if we get an an error message if a name is not set
+	 * Check if we get an an error message if a name has less than 3 characters
 	 */
 	@Test
 	public void NameIsSmallerThan3() {
@@ -45,7 +52,8 @@ public class PatientsTest {
 	}
 
 	/**
-	 * Check if we get an an error message if a name is not set
+	 * Check if we get an an error message if a surname is less than 3
+	 * characters
 	 */
 	@Test
 	public void SurnameIsSmallerThan3() {
@@ -69,25 +77,151 @@ public class PatientsTest {
 		assertThat(warning).isEqualTo(newAdd.validate());
 	}
 
+	@Before
+	public void setupLogin() {
+		running(fakeApplication(), new Runnable() {
+			public void run() { // Login
+				final Map<String, String> data = new HashMap<String, String>();
+				data.put("username", "user");
+				data.put("password", "pass");
+				result = callAction(
+						controllers.routes.ref.Authentication.authenticate(),
+						fakeRequest().withFormUrlEncodedBody(data));
+				// Recover cookie from login result
+				playSession = play.test.Helpers.cookie("PLAY_SESSION", result);
+			}
+		});
+	}
+
+	/**
+	 * Test that when a logged in user wants to see all patients, it really
+	 * arrives at the patients overview page
+	 * 
+	 */
 	@Test
 	public void testPatientsControllerShowAll() {
 		running(fakeApplication(), new Runnable() {
-			public void run() { // LOGIN
+			public void run() { // Login
 				final Map<String, String> data = new HashMap<String, String>();
 				data.put("username", "user");
 				data.put("password", "pass");
 				Result result = callAction(
 						controllers.routes.ref.Authentication.authenticate(),
 						fakeRequest().withFormUrlEncodedBody(data));
-				// RECOVER COOKIE FROM LOGIN RESULT
+				// Recover cookie from login result
 				final Cookie playSession = play.test.Helpers.cookie(
 						"PLAY_SESSION", result);
-				// LIST SOMETHING (using cookie of the login result)
+				// List something (using cookie of the login result)
 				result = callAction(controllers.routes.ref.Patients.showAll(),
 						fakeRequest().withCookies(playSession));
 				assertThat(status(result)).isEqualTo(OK);
 				assertThat(contentAsString(result)).contains(
 						"Patients overview");
+			}
+		});
+	}
+
+	/**
+	 * Test that when a non excisting patient is called, the user get's a 404
+	 * page
+	 */
+	@Test
+	public void testPatientsControllerPatientNotFound() {
+		running(fakeApplication(), new Runnable() {
+			public void run() { // Login
+				final Map<String, String> data = new HashMap<String, String>();
+				data.put("username", "user");
+				data.put("password", "pass");
+				Result result = callAction(
+						controllers.routes.ref.Authentication.authenticate(),
+						fakeRequest().withFormUrlEncodedBody(data));
+				// Recover cookie from login result
+				final Cookie playSession = play.test.Helpers.cookie(
+						"PLAY_SESSION", result);
+				int nonExistingPatientID = Integer.MAX_VALUE;
+				// List something (using cookie of the login result)
+				result = callAction(controllers.routes.ref.Patients
+						.show(nonExistingPatientID),
+						fakeRequest().withCookies(playSession));
+				assertThat(status(result)).isEqualTo(NOT_FOUND);
+			}
+		});
+	}
+
+	/**
+	 * Test that when a client wants to add a patient, the correct fields are
+	 * shown
+	 */
+	@Test
+	public void testPatientsControllerAddPatient() {
+		running(fakeApplication(), new Runnable() {
+			public void run() { // Login
+				final Map<String, String> data = new HashMap<String, String>();
+				data.put("username", "user");
+				data.put("password", "pass");
+				Result result = callAction(
+						controllers.routes.ref.Authentication.authenticate(),
+						fakeRequest().withFormUrlEncodedBody(data));
+				// Recover cookie from login result
+				final Cookie playSession = play.test.Helpers.cookie(
+						"PLAY_SESSION", result);
+				// Request the add page
+				result = callAction(controllers.routes.ref.Patients.add(),
+						fakeRequest().withCookies(playSession));
+				assertThat(status(result)).isEqualTo(OK);
+				assertThat(contentAsString(result)).contains("Add new patient");
+			}
+		});
+	}
+
+	/**
+	 * Test that when a client wants to add a patient without filling in the
+	 * forms, a http 400 error is shown that
+	 */
+	@Test
+	public void testPatientsControllerAddInvalidPatient() {
+		running(fakeApplication(), new Runnable() {
+			public void run() { // Login
+				final Map<String, String> data = new HashMap<String, String>();
+				data.put("username", "user");
+				data.put("password", "pass");
+				Result result = callAction(
+						controllers.routes.ref.Authentication.authenticate(),
+						fakeRequest().withFormUrlEncodedBody(data));
+				// Recover cookie from login result
+				final Cookie playSession = play.test.Helpers.cookie(
+						"PLAY_SESSION", result);
+				// Request the add page
+				result = callAction(controllers.routes.ref.Patients.insert(),
+						fakeRequest().withCookies(playSession));
+				assertThat(status(result)).isEqualTo(BAD_REQUEST);
+			}
+		});
+	}
+
+	/**
+	 * Test that when a client wants to remove a patient that does not exist, a
+	 * http 400 error is shown
+	 */
+	@Test
+	public void testPatientsControllerRemoveInvalidPatient() {
+		running(fakeApplication(), new Runnable() {
+			public void run() { // Login
+				final Map<String, String> data = new HashMap<String, String>();
+				data.put("username", "user");
+				data.put("password", "pass");
+				Result result = callAction(
+						controllers.routes.ref.Authentication.authenticate(),
+						fakeRequest().withFormUrlEncodedBody(data));
+				int nonExistingPatientID = Integer.MAX_VALUE;
+				// Recover cookie from login result
+				final Cookie playSession = play.test.Helpers.cookie(
+						"PLAY_SESSION", result);
+				// Request the add page
+				result = callAction(controllers.routes.ref.Patients
+						.remove(nonExistingPatientID), fakeRequest()
+						.withCookies(playSession));
+				assertThat(status(result)).isEqualTo(BAD_REQUEST);
 			}
 		});
 	}
