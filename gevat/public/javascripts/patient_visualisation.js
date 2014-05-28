08ab0ca2d772502c3d56eb6308e3cc159ee0ec8b
Mathijs Hoogland <mathijs@hoogland.it>
2014-05-28 19:16:38 +0200

    Updated the highlighting of chromosomes that have mutations

--- gevat/public/javascripts/patient_visualisation.js
+++ gevat/public/javascripts/patient_visualisation.js
@@ -11,7 +11,7 @@ $( document ).ready(function() {
 
     function getChromosomesWithMutations() {
         for (var r = 1, n = table.rows.length; r < n; r++) {
-            chromosomesWithMutations.push("chromosome"+table.rows[r].cells[2].innerHTML);
+            chromosomesWithMutations.push("chromosome" + table.rows[r].cells[2].innerHTML);
         }
     }
 
@@ -22,41 +22,44 @@ $( document ).ready(function() {
         .enter()
         .append("span")
         .append("svg")
-        .attr("width", 25)
+        .attr("width", 15)
         .attr("height", 100)
         .append("rect")
-        .attr("class", function(d){return "chromosome-visualisation chromosome" + d;})
-        .attr("data-chr-id", function(d){return d;})
-        .style("fill", "black")
+        .attr("class", function (d) {
+            return "chromosome-visualisation chromosome" + d;
+        })
+        .attr("data-chr-id", function (d) {
+            return d;
+        })
+
         .attr("x", 5)
         .attr("rx", 10)
         .attr("ry", 10)
         .attr("width", 10)
         .attr("height", 100)
-        .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
-        .on("mouseout", function(){d3.select(this).style("fill", "black");});
 
-    for(var i = 0; i < chromosomesWithMutations.length; i++) {
-        d3.selectAll("." + chromosomesWithMutations[i]).style("fill", "red")
-        .on("mouseout", function(){d3.select(this).style("fill", "red");});
+    ;
+
+    function markMutations() {
+        for (var i = 0; i < chromosomesWithMutations.length; i++) {
+            d3.selectAll("." + chromosomesWithMutations[i]).attr("class", function (d) {
+                return "chromosome-visualisation mutation chromosome" + d;
+            });
+        }
     }
+    markMutations();
 
     // Highlight mutations in the table while hovering over a chromosome
     $(".chromosome-visualisation").hover(function() {
         var chrId = $(this).data("chr-id");
         //alert("hoi" + chrId);
         $(".table-mutations tr[data-chr-id=" + chrId + "]").addClass("highlight");
-        //$("chromosome" + chrId).style("fill", "black");
-        d3.selectAll(".chromosome" + chrId).style("fill", "aliceblue");
+        d3.selectAll(".chromosome" + chrId).attr("class", function(d){return "chromosome-visualisation highlight chromosome" + d;});
         //alert("chromosome" + chrId);
     }, function() {
         var chrId = $(this).data("chr-id");
         $(".table-mutations tr[data-chr-id=" + chrId + "]").removeClass("highlight");
-        //TODO: niet zo netjes...
-        var kleur = "black";
-        if(chromosomesWithMutations.indexOf("chromosome" + chrId) != -1)  {
-            kleur = "red";
-        }
-        d3.selectAll(".chromosome" + chrId).style("fill", kleur);
+        d3.selectAll(".chromosome" + chrId).attr("class", function(d){return "chromosome-visualisation chromosome" + d;});
+        markMutations();
     });
 });
