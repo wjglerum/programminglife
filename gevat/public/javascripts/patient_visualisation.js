50fcf019ac3654eb346fbe1c10ccaa4fa5f29720
Ruben <rubenbes94@gmail.com>
2014-06-02 15:19:47 +0200

    Finished highlighting the table when hovering a mutation

--- gevat/public/javascripts/patient_visualisation.js
+++ gevat/public/javascripts/patient_visualisation.js
@@ -1,40 +1,10 @@
 $(document).ready(function () {
-
     var chromosomes = [];
     for (var i = 1; i < 24; i++) {
         chromosomes.push(i);
     }
     var chromosomesWithMutations = [];
     var table = document.getElementById("table-mutations");
-
-    function getChromosomesWithMutations() {
-        for (var r = 1, n = table.rows.length; r < n; r++) {
-            chromosomesWithMutations.push("chromosome" + table.rows[r].cells[2].innerHTML);
-        }
-    }
-
-    function getMutationsFromChromosome(i) {
-        var output = "";
-        // Loop over de tabel om te kijken of er mutations zijn in chromosoom i
-        for (var r = 1, n = table.rows.length; r < n; r++) {
-            if (table.rows[r].cells[2].innerHTML == i) {
-                var url = table.rows[r].getAttribute("href");
-                var chr = table.rows[r].getAttribute("data-chr-id");
-                var snp = table.rows[r].cells[1].innerHTML;
-                var mutationid = table.rows[r].getAttribute("data-mutation-id");
-                output += "<li><a class=\"mutation-list-item\" data-chr-id=" + chr + " data-mutation-id=" + mutationid + " href=" + url + ">" + snp + "</a></li>";
-            }
-        }
-        return output;
-    }
-
-    function markMutations() {
-        for (var i = 0; i < chromosomesWithMutations.length; i++) {
-            d3.selectAll("." + chromosomesWithMutations[i]).attr("class", function (d) {
-                return "chromosome-visualisation mutation " + chromosomesWithMutations[i];
-            });
-        }
-    }
 
     //Make a table for our chromosomes, and initiate the first table row (for the svg's)
     d3.select("#patient_visualisation")
@@ -64,7 +34,6 @@ $(document).ready(function () {
                 .attr("height", 100);
         }
     }
-
     // Add numbers to the chromosomes
     var tableDescription = d3.select(".chromosome-pair-table")
         .append("tr")
@@ -77,7 +46,7 @@ $(document).ready(function () {
 
     getChromosomesWithMutations();
     markMutations();
-
+    
     // Actions when hovering over a table cell containing two chromosomes
     $(".chromosome-pair-table-cel").hover(function () {
         var chrId = $(this).data("chr-id");
@@ -90,7 +59,7 @@ $(document).ready(function () {
         // If there are mutations at a certain chromosome number, show them, else tell that there are no mutations
         var mutations = (getMutationsFromChromosome(chrId) == "") ? "<p>There are no mutations in chromosome " + chrId + "</p>" : "<ul>" + getMutationsFromChromosome(chrId) + "</ul>";
         d3.select("#patient_chromosome_information").html("<h3>Mutations in chromosome " + chrId + "</h3>" + mutations);
-    }, function () {
+        }, function () {
         // Make sure the chromosomes get their normal color back when you stop hovering over them
         var chrId = $(this).data("chr-id");
         $(".table-mutations tr[data-chr-id=" + chrId + "]").removeClass("highlight");
@@ -98,18 +67,7 @@ $(document).ready(function () {
             return "chromosome-visualisation chromosome" + chrId;
         });
         markMutations();
-    });
-
-//TODO: dit aan de praat krijgen...
-//    // Actions when hovering over mutations list
-//    $(".mutation-list-item").hover(function(){
-//        //var mutationId = $(this).data("mutation-id");
-//        //$(".table-mutations tr[data-mutation-id=" + mutationId + "]").addClass("highlight");
-//    }, function() {
-//        var mutationId = $(this).data("mutation-id");
-//
-//        $(".table-mutations tr[data-mutation-id=" + mutationId + "]").removeClass("highlight");
-//    });
+    });    
 
     // Actions when hovering over table
     $(".table-mutation").hover(function () {
@@ -121,5 +79,44 @@ $(document).ready(function () {
         d3.selectAll(".chromosome" + chrId).attr("class", "chromosome-visualisation mutation chromosome" + chrId);
     });
 
+    // Actions when hovering over the mutations
+    $(".page-content").on({
+        mouseenter: function () {
+            var id = $(this)[0].getAttribute("data-mutation-id");
+            $(".table-mutations tr[data-mutation-id=" + id + "]").addClass("highlight");
+        },
+        mouseleave: function() {
+            var id = $(this)[0].getAttribute("data-mutation-id");
+            $(".table-mutations tr[data-mutation-id=" + id + "]").removeClass("highlight");
+        }
+    }, ".mutation-list-item");
 
+    function getChromosomesWithMutations() {
+            for (var r = 1, n = table.rows.length; r < n; r++) {
+                chromosomesWithMutations.push("chromosome" + table.rows[r].cells[2].innerHTML);
+            }
+        }
+
+    function getMutationsFromChromosome(i) {
+        var output = "";
+        // Loop over de tabel om te kijken of er mutations zijn in chromosoom i
+        for (var r = 1, n = table.rows.length; r < n; r++) {
+            if (table.rows[r].cells[2].innerHTML == i) {
+                var url = table.rows[r].getAttribute("href");
+                var chr = table.rows[r].getAttribute("data-chr-id");
+                var snp = table.rows[r].cells[1].innerHTML;
+                var mutationid = table.rows[r].getAttribute("data-mutation-id");
+                output += "<li><a class=\"mutation-list-item\" data-chr-id=" + chr + " data-mutation-id=" + mutationid + " href=" + url + ">" + snp + "</a></li>";
+            }
+        }
+        return output;
+    }
+
+    function markMutations() {
+        for (var i = 0; i < chromosomesWithMutations.length; i++) {
+            d3.selectAll("." + chromosomesWithMutations[i]).attr("class", function (d) {
+                return "chromosome-visualisation mutation " + chromosomesWithMutations[i];
+            });
+        }
+    }
 });
