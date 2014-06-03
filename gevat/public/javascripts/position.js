dbb81d0f768b8112107101aa017d7736391a632e
Willem Jan Glerum <wjglerum@gmail.com>
2014-06-03 15:12:26 +0200

    The markers are now relate to each other

--- gevat/public/javascripts/position.js
+++ gevat/public/javascripts/position.js
@@ -1,10 +1,34 @@
+var left = $(".position").data("left");
+var right = $(".position").data("right");
+var marker = $(".position").data("marker");	
+
+var leftBar;
+var rightBar;
+var markerBar;
+
+if(marker < left) {
+	markerBar = "10%";
+	leftBar = Math.floor((left-marker)/(right-marker)*100) + "%";
+} else {
+	leftBar = "10%";
+	markerBar = Math.floor((marker-left)/(right-left)*100) + "%";
+}
+
+if(marker > right) {
+	markerBar = "90%";
+	rightBar = Math.floor((right-left)/(marker-left)*100) + "%";
+} else {
+	rightBar = "90%";
+	markerBar = Math.floor((marker-left)/(right-left)*100) + "%";
+}
+
 var svgContainer = d3.select("div.position")
 	.append("svg")
 	.attr("width", "100%")
 	.attr("height", 200);
 
 var bar = svgContainer.append("rect")
-	.attr("x", 5)
+	.attr("x", "5%")
 	.attr("y", 100)
 	.attr("rx", 20)
 	.attr("ry", 20)
@@ -14,26 +38,26 @@ var bar = svgContainer.append("rect")
 	.attr("stroke", "black")
 	.attr("fill-opacity", 0.1);
 
-var marker = svgContainer.append("line")
-	.attr("x1", "50%")
+var mutation = svgContainer.append("line")
+	.attr("x1", markerBar)
 	.attr("y1", 100)
-	.attr("x2", "50%")
+	.attr("x2", markerBar)
 	.attr("y2", 125)
 	.attr("stroke-width", 10)
 	.attr("stroke", "red");
 	
 var edge1 = svgContainer.append("line")
-	.attr("x1", "40%")
+	.attr("x1", leftBar)
 	.attr("y1", 100)
-	.attr("x2", "40%")
+	.attr("x2", leftBar)
 	.attr("y2", 125)
 	.attr("stroke-width", 5)
 	.attr("stroke", "black");
 	
 var edge2 = svgContainer.append("line")
-	.attr("x1", "70%")
+	.attr("x1", rightBar)
 	.attr("y1", 100)
-	.attr("x2", "70%")
+	.attr("x2", rightBar)
 	.attr("y2", 125)
 	.attr("stroke-width", 5)
 	.attr("stroke", "black");
