566ada3ebbe3b93c2236f97838632b84a6cef043
Willem Jan Glerum <wjglerum@gmail.com>
2014-06-03 15:54:25 +0200

    Updated position.js

--- gevat/public/javascripts/position.js
+++ gevat/public/javascripts/position.js
@@ -1,3 +1,4 @@
+//retrieve data from html
 var left = $(".position").data("left");
 var right = $(".position").data("right");
 var marker = $(".position").data("marker");	
@@ -6,27 +7,36 @@ var leftBar;
 var rightBar;
 var markerBar;
 
+
+//check where the marker is, on the left, right or middle
 if(marker < left) {
 	markerBar = "10%";
-	leftBar = Math.floor((left-marker)/(right-marker)*100) + "%";
+	leftBar = Math.floor((left - marker) / (right - marker) * 100);
+	leftBar += "%";
 } else {
 	leftBar = "10%";
-	markerBar = Math.floor((marker-left)/(right-left)*100) + "%";
+	markerBar = Math.floor((marker - left) / (right - left) * 100);
+	markerBar += "%";
 }
 
 if(marker > right) {
 	markerBar = "90%";
-	rightBar = Math.floor((right-left)/(marker-left)*100) + "%";
+	rightBar = Math.floor((right - left) / (marker - left) * 100);
+	rightBar += "%";
 } else {
 	rightBar = "90%";
-	markerBar = Math.floor((marker-left)/(right-left)*100) + "%";
+	markerBar = Math.floor((marker - left) / (right - left) * 100);
+	markerBar += "%";
 }
 
+//make a container for svg
 var svgContainer = d3.select("div.position")
 	.append("svg")
 	.attr("width", "100%")
 	.attr("height", 200);
 
+
+//make the bar
 var bar = svgContainer.append("rect")
 	.attr("x", "5%")
 	.attr("y", 100)
@@ -38,6 +48,8 @@ var bar = svgContainer.append("rect")
 	.attr("stroke", "black")
 	.attr("fill-opacity", 0.1);
 
+
+//draw the red mutation
 var mutation = svgContainer.append("line")
 	.attr("x1", markerBar)
 	.attr("y1", 100)
@@ -46,6 +58,7 @@ var mutation = svgContainer.append("line")
 	.attr("stroke-width", 10)
 	.attr("stroke", "red");
 	
+//draw the left edge of gene
 var edge1 = svgContainer.append("line")
 	.attr("x1", leftBar)
 	.attr("y1", 100)
@@ -54,6 +67,8 @@ var edge1 = svgContainer.append("line")
 	.attr("stroke-width", 5)
 	.attr("stroke", "black");
 	
+	
+//draw the right edge of gene
 var edge2 = svgContainer.append("line")
 	.attr("x1", rightBar)
 	.attr("y1", 100)
