8ebd7d42229af8e667ce429a32482a88705427d1
Willem Jan Glerum <wjglerum@gmail.com>
2014-06-04 13:45:08 +0200

    Small fix in database

--- gevat/app/models/Database.java
+++ gevat/app/models/Database.java
@@ -36,13 +36,13 @@ public class Database {
 	 * @param query
 	 *            The query to be executed, in string format
 	 * @return ResultSet ResultSet containing the results of the query
-	 * @throws SQLException 
+	 * @throws SQLException
 	 */
-	public static ResultSet select(final String database, final String query) throws SQLException {
+	public static ResultSet select(final String database, final String query)
+			throws SQLException {
 		try (Connection connection = DB.getConnection(database);) {
 			return connection.createStatement().executeQuery(query);
 		}
-		connection.close();
 	}
 
 	/**
