58ac2dd0f07c0ab1295ea7cd3853ff10db1f96e5
Jasper Denkers <git@jasperdenkers.com>
2014-06-16 21:19:07 +0200

    Fixed tests by removing tests for help and about page

--- gevat/test/test/controllers/ApplicationTest.java
+++ gevat/test/test/controllers/ApplicationTest.java
@@ -31,30 +31,6 @@ public class ApplicationTest {
 	}
 
 	/**
-	 * Test the help page.
-	 */
-	@Test
-	public void testHelp() {
-		Result result = callAction(controllers.routes.ref.Application.help());
-		assertThat(status(result)).isEqualTo(OK);
-		assertThat(contentType(result)).isEqualTo("text/html");
-		assertThat(charset(result)).isEqualTo("utf-8");
-		assertThat(contentAsString(result)).contains("Help");
-	}
-
-	/**
-	 * Test the about page.
-	 */
-	@Test
-	public void testAbout() {
-		Result result = callAction(controllers.routes.ref.Application.about());
-		assertThat(status(result)).isEqualTo(OK);
-		assertThat(contentType(result)).isEqualTo("text/html");
-		assertThat(charset(result)).isEqualTo("utf-8");
-		assertThat(contentAsString(result)).contains("About");
-	}
-
-	/**
 	 * Test the JS routes.
 	 */
 	@Test
