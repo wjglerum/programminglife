afe8e13fd6a833fb808a3f7e20c9c16a1c98df32
robbertvs <robbertvs@gmail.com>
2014-06-16 15:21:18 +0200

    added getters and setters to subclass Add

--- gevat/app/controllers/Patients.java
+++ gevat/app/controllers/Patients.java
@@ -101,6 +101,30 @@ public class Patients extends Controller {
 	public static class Add {
 
 		private String name;
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getSurname() {
+			return surname;
+		}
+
+		public void setSurname(String surname) {
+			this.surname = surname;
+		}
+
+		public String getSex() {
+			return sex;
+		}
+
+		public void setSex(String sex) {
+			this.sex = sex;
+		}
+
 		private String surname;
 		private String sex;
 
