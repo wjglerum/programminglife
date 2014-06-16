e5c6a29a5e91921fd03a50f395fc8a8d7f55bab6
Willem Jan Glerum <wj.glerum@gmail.com>
2014-06-16 15:15:51 +0200

    Reverted ProteinGrahp due to static related errors.

--- gevat/app/models/protein/ProteinRepository.java
+++ gevat/app/models/protein/ProteinRepository.java
@@ -2,7 +2,6 @@ package models.protein;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collection;
 
 import models.mutation.Mutation;
 import models.patient.Patient;
@@ -18,20 +17,4 @@ public interface ProteinRepository {
 
 	public ArrayList<Mutation> getRelatedMutations(Patient p, Mutation m)
 			throws SQLException;
-
-	public void addConnectionsOfSnp(int snp, int limit,
-			int threshold) throws SQLException;
-
-	public void addDistantConnectionsOfSnp(int snp, int limit,
-			int threshold, int distance) throws SQLException;
-
-	public void addDistantConnectionsOfProtein(String protein,
-			int limit, int threshold, int distance) throws SQLException;
-
-	public Collection<Protein> addConnectionsOfProteine(String protein,
-			int limit, int threshold) throws SQLException;
-
-	public Collection<Protein> addConnections(String p1, String connections);
-
-	public void add(String p1, String p2, int combinedScore);
 }
