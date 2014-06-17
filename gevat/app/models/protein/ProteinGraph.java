cb6e9011ff3343836c1bf55d25c75521392e6301
robbertvs <robbertvs@gmail.com>
2014-06-17 12:02:21 +0200

    Checkstyle for ProteinGraph

--- gevat/app/models/protein/ProteinGraph.java
+++ gevat/app/models/protein/ProteinGraph.java
@@ -2,67 +2,93 @@ package models.protein;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import models.database.QueryProcessor;
 import models.reader.GeneDiseaseLinkReader;
 import play.Logger;
 
 /**
- * 
+ *
  * @author rhvanstaveren
- * 
+ *
  */
 
 public class ProteinGraph {
+	/**
+	 * Maps the name of proteins to the protein object with that name.
+	 */
 	private Map<String, Protein> proteines = new HashMap<String, Protein>();
-	private Collection<ProteinConnection> connections = new ArrayList<ProteinConnection>();
+	/**
+	 * A list of all connections between proteins in this graph.
+	 */
+	private Collection<ProteinConnection> connections =
+			new ArrayList<ProteinConnection>();
 
 	/**
-	 * Creates an empty ProteineGraph
+	 * Creates an empty ProteineGraph.
 	 */
 	public ProteinGraph() {
 	}
 
 	/**
 	 * Creates and ProteineGraph using the proteine the location of the snp.
-	 * 
+	 *
 	 * @param snp
 	 *            the rsid of the location
+	 * @param limit
+	 *            the maximum amount of proteinConnection added per protein
+	 * @param threshold
+	 *            the minimum threshold for an added connection
 	 */
-	public ProteinGraph(int snp, int limit, int threshold) {
+	public ProteinGraph(final int snp, final int limit,
+			final int threshold) {
 		addConnectionsOfSnp(snp, limit, threshold);
 		connectAllProteines();
 	}
 
 	/**
 	 * Creates and ProteineGraph using the proteine the location of the snp.
-	 * 
+	 *
 	 * @param snp
 	 *            the rsid of the location
 	 * @param distance
 	 *            defines the allowed distance to the protein on the snp
 	 *            location
+	 * @param limit
+	 *            the maximum amount of proteinConnection added per protein
+	 * @param threshold
+	 *            the minimum threshold for an added connection
 	 */
-	public ProteinGraph(int snp, int limit, int threshold, int distance) {
+	public ProteinGraph(final int snp, final int limit, final int threshold,
+			final int distance) {
 		addDistantConnectionsOfSnp(snp, limit, threshold, distance);
 		connectAllProteines();
 	}
 
 	/**
-	 * Looks up the proteine at the location of the snp and adds this proteine
-	 * and it's possible connected proteines to ProteineGraph
-	 * 
+	 * Looks up the proteine at the location of the snp.
+	 * adds this proteine
+	 * and it's possible connected proteines to ProteineGraph.
+	 *
 	 * @param snp
 	 *            the rsid of the location
+	 * @param limit
+	 *            the maximum amount of proteinConnection added per protein
+	 * @param threshold
+	 *            the minimum threshold for an added connection
 	 */
-	public void addConnectionsOfSnp(int snp, int limit, int threshold) {
+	public void addConnectionsOfSnp(final int snp, final int limit,
+			final int threshold) {
 		try {
-			ArrayList<String> qResult = QueryProcessor
-					.findGenesAssociatedWithSNP(snp);
-			for (String protein : QueryProcessor.
-					findGenesAssociatedWithSNP(snp)) {
-				addConnectionsOfProteine(protein, limit, threshold);
+			for (String protein : QueryProcessor
+					.findGenesAssociatedWithSNP(snp)) {
+				addConnectionsOfProteine(protein,
+						limit, threshold);
 			}
 		} catch (SQLException e) {
 			Logger.info(e.toString());
@@ -70,24 +96,27 @@ public class ProteinGraph {
 	}
 
 	/**
-	 * Looks up the proteine at the location of the snp and adds this proteine
-	 * and it's possible connected proteines to ProteineGraph
-	 * 
+	 * Looks up the proteine at the location of the snp.
+	 * Adds this proteine and it's possible connected proteines
+	 * to ProteineGraph
+	 *
 	 * @param snp
 	 *            the rsid of the location
 	 * @param distance
-	 *            the maximum amount of connections of an protein added to the
-	 *            graph
+	 *            the maximum amount of connections of an protein added to
+	 *            the graph
+	 * @param limit
+	 *            the maximum amount of proteinConnection added per protein
+	 * @param threshold
+	 *            the minimum threshold for an added connection
 	 */
-	public void addDistantConnectionsOfSnp(int snp, int limit, int threshold,
-			int distance) {
+	public void addDistantConnectionsOfSnp(final int snp, final int limit,
+			final int threshold, final int distance) {
 		try {
-			ArrayList<String> qResult = QueryProcessor
-					.findGenesAssociatedWithSNP(snp);
-			for (String protein : QueryProcessor.
-					findGenesAssociatedWithSNP(snp)) {
-				addDistantConnectionsOfProtein(protein, limit, threshold,
-						distance);
+			for (String protein : QueryProcessor
+					.findGenesAssociatedWithSNP(snp)) {
+				addDistantConnectionsOfProtein(protein, limit,
+						threshold, distance);
 			}
 		} catch (SQLException e) {
 			Logger.info(e.toString());
@@ -95,40 +124,56 @@ public class ProteinGraph {
 	}
 
 	/**
-	 * Adds the proteine and it's possible connected proteines to ProteineGraph
-	 * 
+	 * Adds the proteine and it's possible connected proteines to
+	 * ProteineGraph.
+	 *
 	 * @param protein
+	 *            A proteine to find neighbours of
 	 * @param distance
-	 *            the maximum amount of connections of an protein added to the
-	 *            graph
+	 *            the maximum amount of connections of an protein added to
+	 *            the graph
+	 * @param limit
+	 *            the maximum amount of proteinConnection added per protein
+	 * @param threshold
+	 *            the minimum threshold for an added connection
 	 */
-	public void addDistantConnectionsOfProtein(String protein, int limit,
-			int threshold, int distance) {
+	public void addDistantConnectionsOfProtein(final String protein,
+			final int limit, final int threshold,
+			final int distance) {
 		Collection<Protein> currProteins = new HashSet<Protein>();
 		currProteins.add(getProtein(protein));
-		while (distance-- > 0) {
-			Collection<Protein> newProteins = new HashSet<Protein>();
+		for (int d = 0; d < distance; d++) {
+			Collection<Protein> newProteins =
+					new HashSet<Protein>();
 			for (Protein p : currProteins) {
-				newProteins.addAll(addConnectionsOfProteine(p.getName(), limit,
-						threshold));
+				newProteins.addAll(addConnectionsOfProteine(
+						p.getName(), limit, threshold));
 			}
 			currProteins = newProteins;
-			Logger.info("ADCOP:\t" + distance + "\t" + currProteins.size());
+			Logger.info("ADCOP:\t" + distance + "\t"
+				+ currProteins.size());
 		}
 	}
 
 	/**
-	 * adds the proteine and it's possible connected proteines to ProteineGraph
-	 * 
-	 * @param proteine
+	 * adds the proteine and it's possible connected proteines to
+	 * ProteineGraph.
+	 *
+	 * @param protein
 	 *            A proteine to find neighbours of
+	 * @param limit
+	 *            the maximum amount of proteinConnection added per protein
+	 * @param threshold
+	 *            the minimum threshold for an added connection
 	 * @return gives back the proteins that have been added to the graph
 	 */
-	public Collection<Protein> addConnectionsOfProteine(String protein,
-			int limit, int threshold) {
+	public Collection<Protein> addConnectionsOfProteine(
+			final String protein, final int limit,
+			final int threshold) {
 		try {
-			return addConnections(protein, QueryProcessor.findGeneConnections(
-					protein, limit, threshold));
+			return addConnections(protein, QueryProcessor.
+					findGeneConnections(protein, limit,
+							threshold));
 		} catch (SQLException e) {
 			Logger.info(e.toString());
 			return new ArrayList<Protein>();
@@ -137,34 +182,39 @@ public class ProteinGraph {
 
 	/**
 	 * Add proteine p1 and it's connections with proteines in connections to
-	 * ProteineGraph
-	 * 
+	 * ProteineGraph.
+	 *
 	 * @param p1
 	 *            a proteine
-	 * @param connections
+	 * @param connectionsString
 	 *            a string in the format of
 	 *            "[proteine1\tcombinedscore1,...proteineN\tcombinedscoreN]"
 	 * @return gives back the proteins that have been added to the graph
 	 */
-	private Collection<Protein> addConnections(String p1, String connections) {
+	private Collection<Protein> addConnections(final String p1,
+			final String connectionsString) {
 		Collection<Protein> newProteins = new ArrayList<Protein>();
-		if (connections.length() == 0)
+		if (connectionsString.length() == 0) {
 			return newProteins;
-		for (String s : connections.substring(1, connections.length() - 1)
+		}
+		for (String s : connectionsString.substring(1,
+				connectionsString.length() - 1)
 				.split(",")) {
-		    if (!s.isEmpty()) {
-	            String p2 = s.split("\t")[0].trim();
-	            if (!hasProtein(p2))
-	                newProteins.add(getProtein(p2));
-	            add(p1, p2, Integer.parseInt(s.split("\t")[1].trim()));		        
-		    }
+			if (!s.isEmpty()) {
+				String p2 = s.split("\t")[0].trim();
+				if (!hasProtein(p2)) {
+					newProteins.add(getProtein(p2));
+				}
+				add(p1, p2, Integer.parseInt(s.split("\t")[1].
+						trim()));
+			}
 		}
 		return newProteins;
 	}
 
 	/**
-	 * Add a connection between p1 and p2 to ProteineGraph
-	 * 
+	 * Add a connection between p1 and p2 to ProteineGraph.
+	 *
 	 * @param p1
 	 *            proteine one
 	 * @param p2
@@ -172,7 +222,8 @@ public class ProteinGraph {
 	 * @param combinedScore
 	 *            the combined score between proteine one and two
 	 */
-	private void add(String p1, String p2, int combinedScore) {
+	private void add(final String p1, final String p2,
+			final int combinedScore) {
 		ProteinConnection pc = new ProteinConnection(getProtein(p1),
 				getProtein(p2), combinedScore);
 		if (!this.connections.contains(pc)) {
@@ -181,32 +232,47 @@ public class ProteinGraph {
 	}
 
 	/**
-	 * returns the proteine with this name. If this proteine does not excist in
-	 * ProteineGraph a new proteine is made
-	 * 
+	 * returns the proteine with this name.
+	 * If this proteine does not excist in ProteineGraph a new protein is
+	 * made.
+	 *
 	 * @param name
+	 *            the name of the protein to return
 	 * @return the proteine with this name
 	 */
-	public Protein getProtein(String name) {
-		if (!hasProtein(name))
+	public Protein getProtein(final String name) {
+		if (!hasProtein(name)) {
 			try {
-				proteines.put(
-						name,
-						new Protein(name, GeneDiseaseLinkReader
-								.findGeneDiseaseAssociation(name)));
+				proteines.put(name,
+					new Protein(name, GeneDiseaseLinkReader
+						.findGeneDiseaseAssociation(
+								name)));
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		}
 		return proteines.get(name);
 	}
 
-	public boolean hasProtein(String name) {
+	/**
+	 * Checks if the graph contains this protein.
+	 * @param name
+	 * 			the name of the protein to look for
+	 * @return
+	 * 			true if the graph contains a protein with this name
+	 */
+	public boolean hasProtein(final String name) {
 		return proteines.containsKey(name);
 	}
 
+	/**
+	 * Returns a string representation of ProteinGraph.
+	 * @return
+	 * 			a string representation of this ProteinGraph
+	 */
 	public String toString() {
-		return "<ProteineGraph: " + proteines.toString() + ">";
+		return "<ProteinGraph: " + proteines.toString() + ">";
 	}
 
 	public Collection<Protein> getProteines() {
@@ -221,16 +287,25 @@ public class ProteinGraph {
 		return proteines.keySet();
 	}
 
+	/**
+	 * Looks for connections between all proteins currently in the graph.
+	 */
 	private void connectAllProteines() {
 		try {
-			ArrayList<String> connectedProteinScores = QueryProcessor
-					.getConnectedProteinScore(getProteinesAsString());
+			ArrayList<String> connectedProteinScores =
+					QueryProcessor.getConnectedProteinScore(
+							getProteinesAsString());
 
-			for (String connectedProteinScore : connectedProteinScores) {
-				String[] proteinsAndScore = connectedProteinScore.split("=");
+			for (String connectedProteinScore
+					: connectedProteinScores) {
+				String[] proteinsAndScore =
+					connectedProteinScore.split("=");
 
-				String[] proteins = proteinsAndScore[0].split("->");
-				int score = Integer.parseInt(proteinsAndScore[1].trim());
+				String[] proteins =
+					proteinsAndScore[0].split("->");
+				int score =
+					Integer.parseInt(proteinsAndScore[1].
+							trim());
 
 				String proteinA = proteins[0].trim();
 				String proteinB = proteins[1].trim();
@@ -241,9 +316,14 @@ public class ProteinGraph {
 			Logger.info(e.toString());
 		}
 	}
-	
-	public void putMutation(String protein)
-	{
+
+	/**
+	 * Marks the protein to contain a mutation.
+	 *
+	 * @param protein
+	 *            The name of the protein that has a mutation
+	 */
+	public void putMutation(final String protein) {
 		this.getProtein(protein).setMutation(true);
 	}
 }
