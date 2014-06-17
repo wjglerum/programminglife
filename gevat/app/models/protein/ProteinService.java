23b7ca55513962f3c32be893d3a7dd0d82d7949b
robbertvs <robbertvs@gmail.com>
2014-06-18 01:14:29 +0200

    checkstyle ProteinService

--- gevat/app/models/protein/ProteinService.java
+++ gevat/app/models/protein/ProteinService.java
@@ -16,14 +16,30 @@ public class ProteinService {
 
 	private final ProteinRepository proteinRepository;
 
+	/**
+	 * @param proteinRepository A proteinRepository
+	 */
 	public ProteinService(final ProteinRepository proteinRepository) {
 		this.proteinRepository = proteinRepository;
 	}
 
-	public String getAnnotations(String name) throws SQLException {
-		return proteinRepository.getAnnotations(name);
+	/**
+	 * Gets the annotations of the protein.
+	 * 
+	 * @param protein The name of the protein
+	 * @return Returns the annotations associated with the protein.
+	 * @throws SQLException SQL Exception
+	 */
+	public String getAnnotations(String protein) throws SQLException {
+		return proteinRepository.getAnnotations(protein);
 	}
 
+	/**
+	 * @param p The patient
+	 * @param m The mutation
+	 * @return A list of related mutations
+	 * @throws SQLException SQL Exception
+	 */
 	public ArrayList<Mutation> getRelatedMutations(Patient p, Mutation m)
 			throws SQLException {
 		return proteinRepository.getRelatedMutations(p, m);
